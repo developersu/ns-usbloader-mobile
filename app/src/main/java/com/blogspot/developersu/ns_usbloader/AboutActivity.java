5f8afbf68ad19bc99441b4e861e2f06649ff6de9
Dmitry Isaenko <developer.su@gmail.com>
2020-08-05 16:41:57 +0300

    Update 'About' tab. Add more information for donations and stuff like this

--- app/src/main/java/com/blogspot/developersu/ns_usbloader/AboutActivity.java
+++ app/src/main/java/com/blogspot/developersu/ns_usbloader/AboutActivity.java
@@ -1,8 +1,12 @@
 package com.blogspot.developersu.ns_usbloader;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,5 +35,36 @@ public class AboutActivity extends AppCompatActivity {
         t4.setMovementMethod(LinkMovementMethod.getInstance());
         final TextView t6 = findViewById(R.id.textView6);
         t6.setMovementMethod(LinkMovementMethod.getInstance());
+
+        ImageView donateLibera = findViewById(R.id.donateLiberaImageView);
+        donateLibera.setOnClickListener(this::donateLiberaOnClickAction);
+
+        ImageView donatePaypal = findViewById(R.id.donatePaypalImageView);
+        donatePaypal.setOnClickListener(this::donatePaypalOnClickAction);
+
+        ImageView donateYandex = findViewById(R.id.donateYandexImageView);
+        donateYandex.setOnClickListener(this::donateYandexOnClickAction);
+    }
+
+    private void donateLiberaOnClickAction(View view){
+        String url = "https://liberapay.com/developersu/donate";
+        createOpenBrowserIntent(url);
+    }
+    private void donatePaypalOnClickAction(View view){
+        String url = "https://www.paypal.me/developersu";
+        createOpenBrowserIntent(url);
+    }
+    private void donateYandexOnClickAction(View view){
+        String url = "https://money.yandex.ru/to/410014301951665";
+        createOpenBrowserIntent(url);
+    }
+    private void createOpenBrowserIntent(String url){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+        intent.setData(Uri.parse(url));
+
+        startActivity(intent);
     }
 }
